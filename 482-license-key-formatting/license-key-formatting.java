bc9da5d1c092a6d0bfb47a3195d5b62f1e362d4e
Rakshu2323 <keerthirakshana230406@gmail.com>
2026-08-27 22:45:15 +0530

    Time: 12 ms (51.98%) | Memory: 46.5 MB (31.64%) - LeetSync

---
+++ 482-license-key-formatting/license-key-formatting.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String licenseKeyFormatting(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            if (c != '-') {
+                if (sb.length() % (k + 1) == k) sb.append('-');
+                sb.append(Character.toUpperCase(c));
+            }
+        }
+        return sb.reverse().toString();
+    }
+}
